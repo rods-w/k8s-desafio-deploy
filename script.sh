63d2446cf6f677c00346783faa824f87c9eb3617
rods-w <rodolfowanderley93@gmail.com>
2022-11-28 21:38:03 -0300

    Arquivos desafio deploy kubernetes

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando as imagens..."
+
+docker build -t rodolfowanderley/projeto-backend:1.0 backend/.
+docker build -t rodolfowanderley/projeto-database:1.0 database/.
+
+echo "Realizando Push das imagens..."
+
+docker push rodolfowanderley/projeto-backend:1.0
+docker push rodolfowanderley/projeto-database:1.0
+
+echo "Criando Serviços no Cluster Kubernetes..."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments..."
+
+kubectl apply -f ./deployment.yml
